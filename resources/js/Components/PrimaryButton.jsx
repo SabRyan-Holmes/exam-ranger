72f4688f768e4376bb9340a7fe9a728f3389f59a
SabRyan-Holmes <sabrian101maulana@gmail.com>
2024-01-02 21:50:48 +0700

    install daisy-ui & ubah primary button component

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -3,8 +3,7 @@ export default function PrimaryButton({ className = '', disabled, children, ...p
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
+                `inline-flex btn glass scale-75 items-center px-4 py-2 h-5 bg-gray-800 border border-transparent rounded-md font-semibold text-lg  text-white  tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${disabled && 'opacity-25'
                 } ` + className
             }
             disabled={disabled}
