359541271ffe9830a8809111f69b87964431fd10
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2024-01-02 14:20:32 +0700

    added admin check

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard-admin', function () {
+    return Inertia::render('AdminPage');
+})->middleware(['auth', 'verified', 'isAdmin'])->name('AdminPage');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
