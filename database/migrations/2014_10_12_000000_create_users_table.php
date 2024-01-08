5046f11afd72be98ce6db5e301320bd37575dec8
SabRyan-Holmes <sabrian101maulana@gmail.com>
2024-01-09 02:14:59 +0700

    CRUD Peserta Selesai + Update tampilan tabel dikit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->integer('phone')->nullable();
             $table->string('profil_pic')->nullable();
-            // $table->json('answer')->nullable();
+            $table->json('answer')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
