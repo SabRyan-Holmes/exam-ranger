eb6edca6066228748dafe9412b605cb921564ec1
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2024-01-19 16:02:37 +0700

    showed subject image on studenthome screen

--- resources/js/Pages/StudentHome.jsx
+++ resources/js/Pages/StudentHome.jsx
@@ -12,6 +12,7 @@ import no_data from "@/../assets/no_data.svg";
 
 
 export default function StudentHome({ auth, subjectExam, submitted, flash }) {
+    console.log(subjectExam)
     const anchor = useRef('subject')
     const [date, setDate] = useState(new Date());
     console.log(subjectExam)
@@ -85,7 +86,14 @@ export default function StudentHome({ auth, subjectExam, submitted, flash }) {
                                         <div className="mb-2 border-card  card w-fit shadow-md hover:bg-primary/30 ">
                                             <div className="m-6 rounded-md   ">
                                                 <div className="card-actions  items-center ">
-                                                    <strong className="mr-1">{subject.name}</strong>
+                                                    <div>
+                                                        {subject.image ? 
+                                                            <img src={"/storage/" + subject.image} alt="" className='max-h-10 mx-auto' />
+                                                            :
+                                                            ""
+                                                        }
+                                                        <strong className="mr-1">{subject.name}</strong>
+                                                    </div>
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" w-8 h-8 stroke-primary">
                                                         <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                                                     </svg>
